db304ae0b5e0f595f47dc59acacae53deaf2e3df
Sascha Turowski <turowski@turtec.de>
2018-06-28 17:21:28 +0200

    first commit

---
+++ android/app/src/main/java/com/white_label_two/MainActivity.java
@@ -0,0 +1,15 @@
+package com.white_label_two;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "white_label_two";
+    }
+}
